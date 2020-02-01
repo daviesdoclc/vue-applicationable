b0020506aab0ae45ce355a6ae9a1bf75d23cc0b3
daviesdoclc <doug@funkyvisions.com>
2020-02-01 12:38:53 -0500

    vuetify it

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark></v-app-bar>
+    <MyAppBar app></MyAppBar>
+    <v-content>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyAppBar from "@/components/MyAppBar"
 
 export default {
-  name: 'app',
+  
+  name: "App",
+
   components: {
-    HelloWorld
-  }
+    MyAppBar
+  },
+
+  data: () => ({
+  })
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyAppBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :style="style" style="background-color: black; position: fixed; width: 100vw;"></div>
+</template>
+
+<script>
+import Applicationable from "vuetify/es5/mixins/applicationable"
+
+export default {
+  
+  name: "MyAppBar",
+
+  mixins: [
+    Applicationable("top", ["computedTop", "computedHeight"])
+  ],
+
+  props: {
+    app: {
+      type: Boolean,
+      default: true
+    }
+  },
+
+  data() {
+    return {
+      top: 0,
+      height: 80
+    }
+  },
+
+  created() {
+    this.top = this.$vuetify.application.top - this.height
+  },
+
+  computed: {
+    style() {
+      return "top: " + this.top + "px; height: " + this.height + "px;"
+    },
+
+    computedTop() {
+      return this.top
+    },
+
+    computedHeight() {
+      return this.height
+    }
+  },
+
+  methods: {
+    updateApplication() {
+      return this.height
+    }
+  }
+}  
+</script>
